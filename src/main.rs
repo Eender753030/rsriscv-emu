8b0159b553175bb5bc6d566b04af2f3e0dbac8d1
Eender753030 <ender303030@gmail.com>
2025-12-28 11:40:08 +0800

    test: First code test . Except rs1 = 42

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use risc_v_emulator::riscv::RiscV;
+
 fn main() {
-    println!("Hello World!");
+    let mut machine = RiscV::new();
+    
+    let code = 0x02A00093;
+
+    if let Err(e) = machine.load_code(code) {
+        eprintln!("Error: {}", e);
+    }
+    
+    if let Err(e) = machine.cycle() {
+        eprintln!("Error: {}", e);
+    }
+
+    machine.print_registers();
 }
