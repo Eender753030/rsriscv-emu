cb8c992dac480f499c95ea2f82e21168768e4133
Eender753030 <ender303030@gmail.com>
2025-12-30 13:44:02 +0800

    feat: Define B-type, J-type, and I-type for jalr

--- src/riscv/instruction.rs
+++ src/riscv/instruction.rs
@@ -3,16 +3,22 @@
 pub enum InstructionKind {
     Itype,
     ItypeLoad,
+    ItypeJump,
     Rtype,
     Stype,
+    Btype,
+    Jtype,
 }
 
 #[derive(Debug)]
 pub enum Instruction {
     Itype {rd: usize, rs1: usize, imm: i32, funct3: u8},
     ItypeLoad {rd: usize, rs1: usize, imm: i32, funct3: u8},
+    ItypeJump {rd: usize, rs1: usize, imm: i32},
     Rtype {rd: usize, rs1: usize, rs2: usize, funct3: u8, funct7: u8},
     Stype {rs1: usize, rs2: usize, imm: i32, funct3: u8},
+    Btype {rs1: usize, rs2: usize, imm: i32, funct3: u8},
+    Jtype {rd: usize, imm: i32},
 }
 
 impl Instruction {
@@ -36,6 +42,15 @@ impl Instruction {
                 }
             },
 
+            InstructionKind::ItypeJump => {
+                Instruction::ItypeJump {
+                    rd:  ((ins >> 7) & 0x1f) as usize,
+                    rs1: ((ins >> 15) & 0x1f) as usize,
+                    imm: ((ins & 0xfff00000) as i32) >> 20,
+                }
+            },
+
+
             InstructionKind::Rtype => {
                 Instruction::Rtype {
                     rd:  ((ins >> 7) & 0x1f) as usize,
@@ -53,7 +68,23 @@ impl Instruction {
                     imm: (((ins & 0xfe000000) as i32) >> 20) | (((ins >> 7) & 0x1f) as i32),
                     funct3: ((ins >> 12) & 0x7) as u8,
                 }
-            }
+            },
+
+            InstructionKind::Btype => {
+                Instruction::Btype { 
+                    rs1: ((ins >> 15) & 0x1f) as usize, 
+                    rs2: ((ins >> 20) & 0x1f) as usize, 
+                    imm: (((ins & 0x80000000) as i32) >> 19) | (((((ins & 0x80) << 4) | ((ins & 0x7e000000) >> 20) | ((ins & 0xf00) >> 7))) as i32), 
+                    funct3: ((ins >> 12) & 0x7) as u8
+                }
+            },
+
+            InstructionKind::Jtype => {
+                Instruction::Jtype { 
+                    rd: ((ins >> 7) & 0x1f) as usize, 
+                    imm: (((ins & 0x80000000) as i32) >> 11) | (((ins & 0xff000) | ((ins & 0x100000) >> 9) | ((ins & 0x7fe00000) >> 20)) as i32)
+                }
+            },
         }
     }
 }
