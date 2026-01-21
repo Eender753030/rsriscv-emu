12f98e79b891a71373a40bcb2ed023dbd337a687
Eender753030 <ender303030@gmail.com>
2026-01-21 10:37:23 +0800

    test: Test get_bits from u32

--- crates/riscv-core/src/isa/opcode.rs
+++ crates/riscv-core/src/isa/opcode.rs
@@ -68,8 +68,35 @@ impl BitsOp<i32> for u32 {
             return 0;
         }
 
-        let mask = if len >= 32 { !0 } else { !0 >> (32 - len) };
-
-        ((*self as i32) << (32 - offset - len) >> offset) & mask
+        (*self as i32) << (32 - offset - len) >> offset
     }
+}
+
+#[cfg(test)]
+mod bits_op_test {
+    use crate::isa::opcode::BitsOp;
+
+    #[test]
+    fn test_get_bits() {
+        let raw: u32 = 0x12345678; 
+        let ones: u32 = 0xffffffff;
+        let a: u32 = 0xaaaaaaaa;
+        let beef: u32 = 0xDEADBEEF;
+
+        assert_eq!(raw.get_bits(16, 12), 0x234);
+        assert_eq!(ones.get_bits(8, 24), 0xffffff);
+        assert_eq!(a.get_bits(1, 31), 0x55555555);
+        assert_eq!(beef.get_bits(0, 16), 0xBEEF);
+    } 
+
+    #[test]
+    fn test_get_bits_signed() {
+        let signed: u32 = 0x80000000;
+        let unsigned: u32 = 0x7fffffff;
+        let f: u32 = 0xffffffff;
+        
+        assert_eq!(signed.get_bits_signed(1, 31) as u32, 0xc0000000);
+        assert_eq!(unsigned.get_bits_signed(1, 31), 0x3fffffff);
+        assert_eq!(f.get_bits_signed(31, 1) as u32, 0xffffffff);
+    } 
 }
