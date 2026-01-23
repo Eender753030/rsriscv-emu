b87cf1519fa4a90a2bb930fe64e76f47c7d01891
Eender753030 <ender303030@gmail.com>
2026-01-23 19:10:20 +0800

    refactor: Let Execption can carry data if needed

--- crates/riscv-core/src/exception.rs
+++ crates/riscv-core/src/exception.rs
@@ -1,17 +1,26 @@
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Exception {
-    InstructionAddressMisaligned = 0,
-    InstructionAccessFault = 1,
-    IllegalInstruction = 2,
-    Breakpoint = 3,
-    LoadAddressMisaligned = 4,
-    LoadAccessFault = 5,
-    StoreAddressMisaligned = 6,
-    EnvironmentCallFromMMode = 11,
+    InstructionAddressMisaligned,
+    InstructionAccessFault,
+    IllegalInstruction(u32),
+    Breakpoint,
+    LoadAddressMisaligned,
+    LoadAccessFault(u32),
+    StoreAddressMisaligned,
+    EnvironmentCallFromMMode,
 }
 
 impl From<Exception> for u32 {
     fn from(value: Exception) -> Self {
-        value as u32
+        match value {
+            Exception::InstructionAddressMisaligned => 0,
+            Exception::InstructionAccessFault => 1,
+            Exception::IllegalInstruction(_) => 2,
+            Exception::Breakpoint => 3,
+            Exception::LoadAddressMisaligned => 4,
+            Exception::LoadAccessFault(_) => 5,
+            Exception::StoreAddressMisaligned => 6,
+            Exception::EnvironmentCallFromMMode => 11,
+        }
     }
 }
