2e2769a70191cb525e072c49609da92edb9429aa
Eender753030 <ender303030@gmail.com>
2026-01-24 19:11:19 +0800

    refactor: Split some code from csr.rs

--- crates/riscv-core/src/core/csr.rs
+++ crates/riscv-core/src/core/csr.rs
@@ -1,66 +1,11 @@
-use modular_bitfield::prelude::*;
+mod mstatus;
+mod satp;
+mod addr;
+
 use crate::{core::privilege::PrivilegeMode, exception::Exception};
-
-#[bitfield]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Mstatus {
-    #[skip] __: B1, 
-    sie: B1, // bit 1
-    #[skip] __: B1,
-    mie: B1, // bit 3
-    #[skip] __: B1,
-    spie: B1, // bit 5
-    #[skip] __: B1,
-    mpie: B1, // bit 7
-    spp: B1, // bit 8
-    #[skip] __: B2,
-    mpp: B2, // bit 12:11
-    #[skip] __: B19,
-}
-
-const M_MODE_MASK: u32 = (1 << 1) | (1 << 3) | (1 << 5) | (3 << 7) | (3 << 11);
-const S_MODE_MASK: u32 = (1 << 1) | (1 << 5) | (1 << 8);
-
-impl Mstatus {
-    fn read_m(&self) -> u32{
-        u32::from(*self)
-    }
-
-    fn read_s(&self) -> u32{
-        u32::from(*self) & S_MODE_MASK
-    }
-
-    fn write_m(&mut self, data: u32) {
-        *self = ((u32::from(*self) & !M_MODE_MASK) | (data & M_MODE_MASK)).into();
-    }
-
-    fn write_s(&mut self, data: u32) {
-        *self = ((u32::from(*self) & !S_MODE_MASK) | (data & S_MODE_MASK)).into();
-    }
-
-
-    fn reset(&mut self) {
-        *self = Self::default()
-    }
-}
-
-impl From<Mstatus> for u32 {
-    fn from(value: Mstatus) -> Self {
-        u32::from_le_bytes(value.into_bytes())
-    }
-}
-
-impl From<u32> for Mstatus {
-    fn from(value: u32) -> Self {
-        Mstatus::from_bytes(value.to_le_bytes())
-    }
-}
-
-impl Default for Mstatus {
-    fn default() -> Self {
-        Mstatus::new()
-    }
-}
+use mstatus::Mstatus;
+use satp::Satp;
+use addr::CsrAddr;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub struct CsrFile {
@@ -69,7 +14,7 @@ pub struct CsrFile {
     scause: u32,
     sscratch: u32,
     stval: u32,
-    satp: u32,
+    satp: Satp,
 
     mstatus: Mstatus,
     medeleg: u32,
@@ -102,7 +47,7 @@ impl CsrFile {
                 CsrAddr::Scause => self.scause,
                 CsrAddr::Stval => self.stval,
                 CsrAddr::Sip => self.mip & self.mideleg,
-                CsrAddr::Satp => self.satp,
+                CsrAddr::Satp => self.satp.into(),
 
                 CsrAddr::Mstatus => self.mstatus.read_m(),
                 CsrAddr::Medeleg => self.medeleg,
@@ -136,7 +81,7 @@ impl CsrFile {
                 CsrAddr::Scause => self.scause = data,
                 CsrAddr::Stval => self.stval = data,
                 CsrAddr::Sip => self.mip = (self.mip & !self.mideleg) | (data & self.mideleg),
-                CsrAddr::Satp => self.satp = data,
+                CsrAddr::Satp => self.satp = data.into(),
 
                 CsrAddr::Mstatus => self.mstatus.write_m(data),
                 CsrAddr::Medeleg => self.medeleg = data,
@@ -170,7 +115,13 @@ impl CsrFile {
 
         let tval = match except_code {
             Exception::IllegalInstruction(raw) => raw,
-            Exception::LoadAccessFault(addr) => addr,
+            Exception::LoadAccessFault(addr) |
+            Exception::StoreAccessFault(addr) |
+            Exception::InstructionAccessFault(addr) | 
+            Exception::LoadPageFault(addr) |
+            Exception::StoreOrAmoPageFault(addr) |
+            Exception::InstructionPageFault(addr)
+            => addr,
             _ => 0
         };
    
@@ -233,10 +184,15 @@ impl CsrFile {
     } 
 
     pub fn reset(&mut self) {
-        self.mstatus.reset();
-        self.mtvec = 0;
-        self.mepc = 0;
-        self.mcause = 0;
+        *self = Self::default()
+    }
+
+    pub fn check_stap(&self) -> Option<u32> {
+        if self.satp.mode() > 0  {
+            Some(self.satp.ppn())
+        } else {
+            None
+        }
     }
 
     pub fn inspect(&self) -> Vec<(String, u32)> {
@@ -250,7 +206,7 @@ impl CsrFile {
             ("scause".to_string(), self.scause),
             ("stval".to_string(), self.stval),
             ("sip".to_string(), self.mip & self.mideleg),
-            ("stap".to_string(), self.satp),
+            ("stap".to_string(), self.satp.into()),
             ("mstatus".to_string(), self.mstatus.read_m()),
             ("medeleg".to_string(), self.medeleg),
             ("mideleg".to_string(), self.mideleg),
@@ -264,69 +220,5 @@ impl CsrFile {
             ("pmpaddr0".to_string(), 0),
             ("mhartid".to_string(), 0),
         ]
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum CsrAddr {
-    Ustatus = 0x000,
-
-    Sstatus = 0x100,
-    Sie = 0x104,
-    Stvec = 0x105,
-    Sscratch = 0x140,
-    Sepc = 0x141,
-    Scause = 0x142,
-    Stval = 0x143,
-    Sip = 0x144,
-    Satp = 0x180,
-
-    Mstatus = 0x300,
-    Medeleg = 0x302,
-    Mideleg = 0x303,
-    Mie = 0x304,
-    Mtvec = 0x305,
-    Mscratch = 0x340,
-    Mepc = 0x341,
-    Mcause = 0x342,
-    Mtval = 0x343,
-    Mip = 0x344,
-    Pmpcfg0 = 0x3a0,
-    Pmpaddr0 = 0x3b0,
-    Mhartid = 0xf14,
-}
-
-impl TryFrom<u16> for CsrAddr {
-    type Error = Exception;
-    fn try_from(value: u16) -> Result<Self, Self::Error> {
-        match value {
-            0x000 => Ok(CsrAddr::Ustatus),
-
-            0x100 => Ok(CsrAddr::Sstatus),
-            0x104 => Ok(CsrAddr::Sie),
-            0x105 => Ok(CsrAddr::Stvec),
-            0x140 => Ok(CsrAddr::Sscratch),
-            0x141 => Ok(CsrAddr::Sepc),
-            0x142 => Ok(CsrAddr::Scause),
-            0x143 => Ok(CsrAddr::Stval),
-            0x144 => Ok(CsrAddr::Sip),
-            0x180 => Ok(CsrAddr::Satp),
-            
-            0x300 => Ok(CsrAddr::Mstatus),
-            0x302 => Ok(CsrAddr::Medeleg),
-            0x303 => Ok(CsrAddr::Mideleg),
-            0x304 => Ok(CsrAddr::Mie),
-            0x305 => Ok(CsrAddr::Mtvec),
-            0x340 => Ok(CsrAddr::Mscratch),
-            0x341 => Ok(CsrAddr::Mepc),
-            0x342 => Ok(CsrAddr::Mcause),
-            0x343 => Ok(CsrAddr::Mtval),
-            0x344 => Ok(CsrAddr::Mip),
-            0x3a0 => Ok(CsrAddr::Pmpcfg0),
-            0x3b0 => Ok(CsrAddr::Pmpaddr0),
-            0xf14 => Ok(CsrAddr::Mhartid), 
-
-            _ => Err(Exception::IllegalInstruction(value as u32)),
-        }
     }
 }
