5c80e55acf5cc0c2766e9877ae6311400e4ab7d6
Eender753030 <ender303030@gmail.com>
2026-01-26 01:48:38 +0800

    test: Rewrite and add new test for decoder.rs

--- crates/riscv-decoder/src/decoder.rs
+++ crates/riscv-decoder/src/decoder.rs
@@ -31,17 +31,27 @@ pub fn decode(raw: u32) -> Result<Instruction, DecodeError> {
         itype @ (OpCode::ItypeAr | OpCode::ItypeLoad | OpCode::ItypeJump | OpCode::ItypeFence | OpCode::ItypeSystem) => {
             let imm = raw.get_bits_signed(20, 12);
             
-            Ok(if let Some(op) = Rv32iOp::decode_itype(itype, funct3, funct7, imm as u16) {
-                Base(op, InstructionData { rd, rs1, rs2, imm })
-            } else if let Some(op) = ZicsrOp::decode(funct3) {
-                Ziscr(op, InstructionData { rd, rs1, rs2, imm })
-            } else if let Some(op) =  ZifenceiOp::decode(funct3) {
-                Zifencei(op, InstructionData { rd, rs1, rs2, imm })
-            }else if let Some(op) =  PrivilegeOp::decode(raw) {
-                Privileged(op)
-            } else {
-                return Err(DecodeError::UnknownInstruction(itype, raw))
-            })
+            if let Some(op) = Rv32iOp::decode_itype(itype, funct3, funct7, imm as u16) {
+                let res = Base(op, InstructionData { rd, rs1, rs2, imm });
+                return Ok(res);
+            } 
+            
+            if itype == OpCode::ItypeSystem && let Some(op) = ZicsrOp::decode(funct3) {
+                let res = Ziscr(op, InstructionData { rd, rs1, rs2, imm });
+                return Ok(res);
+            } 
+            
+            if itype == OpCode::ItypeFence && let Some(op) =  ZifenceiOp::decode(funct3) {
+                let res = Zifencei(op, InstructionData { rd, rs1, rs2, imm });
+                return Ok(res);
+            } 
+            
+            if itype == OpCode::ItypeSystem && let Some(op) =  PrivilegeOp::decode(raw) {
+                let res = Privileged(op);
+                return Ok(res);
+            } 
+                
+            Err(DecodeError::UnknownInstruction(itype, raw))
         },
         // funct7 [6:0] | rs2 [4:0] | rs1 [4:0] | funct3 [2:0]  | rd [4:0] | opcode [6:0]
         rtype @ OpCode::Rtype => {
@@ -99,155 +109,211 @@ pub fn decode(raw: u32) -> Result<Instruction, DecodeError> {
     }
 }
 
-// #[cfg(test)]
-// mod decoder_test {
-//     use crate::exception::Exception;
-//     use crate::isa::instruction::Instruction;
-//     use crate::isa::{decoder, types::*};
+#[cfg(test)]
+mod tests {
+    mod rv32i {
+        use crate::instruction::{Instruction, InstructionData};
+        use crate::instruction::Rv32iOp;
+        use crate::decoder::decode;
 
-//     #[test]
-//     // Test I-type instruction
-//     fn itype_test() {
-//         // addi x10, x0, -2048
-//         let ins1 = 0x80000513;
-//         let except1 = Instruction::Itype(Itype { rd: 10, rs1: 0, funct3: 0, imm: -2048 });
-//         // slti x23, x24, 31
-//         let ins2 = 0x01fc2b93;
-//         let except2 = Instruction::Itype(Itype { rd: 23, rs1: 24, funct3: 2, imm: 31 });
-//         // andi x31, x22, 2047
-//         let ins3 = 0x7ffb7f93;
-//         let except3 = Instruction::Itype(Itype { rd: 31, rs1: 22, funct3: 7, imm: 2047 });
+        fn build_base_data(op: Rv32iOp, rd: u8, rs1: u8, rs2: u8, imm: i32) -> Instruction {
+            let data = InstructionData { rd, rs1, rs2, imm };
+            Instruction::Base(op, data)
+        }
 
-//         assert_eq!(decoder::decode(ins1), Ok(except1));
+        #[test]
+        fn itype_ar_test() {
+            // addi x10, x0, -2048
+            let ins1 = 0x80000513;
+            let expect1 = build_base_data(Rv32iOp::Addi, 10, 0, 0, -2048);
+            
+            // slti x23, x24, 31
+            let ins2 = 0x01fc2b93;
+            let expect2 = build_base_data(Rv32iOp::Slti, 23, 24, 31, 31);
+        
+            assert_eq!(decode(ins1), Ok(expect1));
+            assert_eq!(decode(ins2), Ok(expect2))
+        }
 
-//         assert_eq!(decoder::decode(ins2), Ok(except2));
+        #[test]
+        fn itype_load_test() {
+            // lw x5, 12(x7)
+            let ins1 = 0x00c3a283;
+            let expect1 = build_base_data(Rv32iOp::Lw, 5, 7, 12, 12);
+            // lbu x19, 0(x11)
+            let ins2 = 0x0005c983;
+            let expect2 = build_base_data(Rv32iOp::Lbu, 19, 11, 0, 0);
 
-//         assert_eq!(decoder::decode(ins3), Ok(except3));
-//     }
+            assert_eq!(decode(ins1), Ok(expect1));
+            assert_eq!(decode(ins2), Ok(expect2));
+        }
 
-//     #[test]
-//     // Test I-type Load instruction
-//     fn itype_load_test() {
-//         // lw x5, 12(x7)
-//         let ins1 = 0x00c3a283;
-//         let except1 = Instruction::ItypeLoad(Itype { rd: 5, rs1: 7, funct3: 2, imm: 12 });
-//         // lbu x19, 0(x11)
-//         let ins2 = 0x0005c983;
-//         let except2 = Instruction::ItypeLoad(Itype { rd: 19, rs1: 11, funct3: 4, imm: 0 });
+        #[test]
+        fn itype_jump_test() {
+            // jalr x1, -442(x21)
+            let ins = 0xe46a80e7;
+            let expect = build_base_data(Rv32iOp::Jalr, 1, 21, 6, -442);
 
-//         assert_eq!(decoder::decode(ins1), Ok(except1));
+            assert_eq!(decode(ins), Ok(expect));
+        }
 
-//         assert_eq!(decoder::decode(ins2), Ok(except2));
-//     }
+        #[test]
+        fn itype_sys_test() {
+            // ecall
+            let ins1 = 0x00000073;
+            let expect1 = build_base_data(Rv32iOp::Ecall, 0, 0, 0, 0);
+            // ebreak
+            let ins2 = 0x00100073;
+            let expect2 = build_base_data(Rv32iOp::Ebreak, 0, 0, 1, 1);
 
-//     #[test]
-//     // Test I-type Jump instruction
-//     fn itype_jump_test() {
-//         // jalr x0, 256(x28)
-//         let ins1 = 0x100e0067;
-//         let except1 = Instruction::ItypeJump(Itype { rd: 0, rs1: 28, funct3: 0, imm: 256 });
-//         // jalr x1, -442(x21)
-//         let ins2 = 0xe46a80e7;
-//         let except2 = Instruction::ItypeJump(Itype { rd: 1, rs1: 21, funct3: 0, imm: -442 });
+            assert_eq!(decode(ins1), Ok(expect1));
+            assert_eq!(decode(ins2), Ok(expect2));
+        }
 
-//         assert_eq!(decoder::decode(ins1), Ok(except1));
+        #[test]
+        fn rtype_test() {
+            // sub x13, x15, x18
+            let ins1 = 0x412786b3;
+            let expect1 = build_base_data(Rv32iOp::Sub, 13, 15, 18, 0);
+            // xor x20, x30, x26
+            let ins2 = 0x01af4a33;
+            let expect2 = build_base_data(Rv32iOp::Xor, 20, 30, 26, 0);
 
-//         assert_eq!(decoder::decode(ins2), Ok(except2));
-//     }
+            assert_eq!(decode(ins1), Ok(expect1));
+            assert_eq!(decode(ins2), Ok(expect2));
+        }
 
-//     #[test]
-//     // Test I-type Jump instruction
-//     fn itype_sys_test() {
-//         // ecall
-//         let ins1 = 0x00000073;
-//         let except1 = Instruction::ItypeSys(Itype { rd: 0, rs1: 0, funct3: 0, imm: 0 });
-//         // ebreak
-//         let ins2 = 0x00100073;
-//         let except2 = Instruction::ItypeSys(Itype { rd: 0, rs1: 0, funct3: 0, imm: 1 });
+        #[test]
+        fn stype_test() {
+            // sw x21, 123(x13)
+            let ins1 = 0x0756ada3;
+            let expect1 = build_base_data(Rv32iOp::Sw, 27, 13, 21, 123);
+            // sh x11, -567(x22)
+            let ins2 = 0xdcbb14a3;
+            let expect2 = build_base_data(Rv32iOp::Sh, 9, 22, 11, -567);
+            
+            assert_eq!(decode(ins1), Ok(expect1));
+            assert_eq!(decode(ins2), Ok(expect2));
+        }
 
-//         assert_eq!(decoder::decode(ins1), Ok(except1));
+        #[test]
+        fn jtype_test() {
+            // jal x1, 32
+            let ins = 0x020000ef;
+            let expect = build_base_data(Rv32iOp::Jal, 1, 0, 0, 32);
 
-//         assert_eq!(decoder::decode(ins2), Ok(except2));
-//     }
+            assert_eq!(decode(ins), Ok(expect));
+        }
 
-//     #[test]
-//     // Test I-type Jump instruction
-//     fn rtype_test() {
-//         // sub x13, x15, x18
-//         let ins1 = 0x412786b3;
-//         let except1 = Instruction::Rtype(Rtype { rd: 13, rs1: 15, rs2: 18, funct3: 0, funct7: 32 });
-//         // sll x2, x6, x9
-//         let ins2 = 0x00931133;
-//         let except2 = Instruction::Rtype(Rtype { rd: 2, rs1: 6, rs2: 9, funct3: 1, funct7: 0 });
-//         // xor x20, x30, x26
-//         let ins3 = 0x01af4a33;
-//         let except3 = Instruction::Rtype(Rtype { rd: 20, rs1: 30, rs2: 26, funct3: 4, funct7: 0 });
+        #[test]
+        fn utype_test() {
+            // auipc x29, 0x100
+            let ins1 = 0x00100e97;
+            let expect1 = build_base_data(Rv32iOp::Auipc, 29, 0, 1, 0x100 << 12);
+            // lui x19, 0x20000
+            let ins2 = 0x200009b7;
+            let expect2 = build_base_data(Rv32iOp::Lui, 19, 0, 0, 0x20000 << 12);
 
-//         assert_eq!(decoder::decode(ins1), Ok(except1));
+            assert_eq!(decode(ins1), Ok(expect1));
+            assert_eq!(decode(ins2), Ok(expect2));
+        }
+    }
 
-//         assert_eq!(decoder::decode(ins2), Ok(except2));
+    mod m {
+        use crate::decoder::decode;
+        use crate::instruction::{Instruction, InstructionData};
+        use crate::instruction::MOp;
 
-//         assert_eq!(decoder::decode(ins3), Ok(except3));
-//     }
+        fn build_m_data(op: MOp, rd: u8, rs1: u8, rs2: u8, imm: i32) -> Instruction {
+            let data = InstructionData { rd, rs1, rs2, imm };
+            Instruction::M(op, data)
+        }
 
-//     #[test]
-//     // Test I-type Jump instruction
-//     fn stype_test() {
-//         // sw x21, 123(x13)
-//         let ins1 = 0x0756ada3;
-//         let except1 = Instruction::Stype(Stype { rs1: 13, rs2: 21, funct3: 2, imm: 123 });
-//         // sh x11, -567(x22)
-//         let ins2 = 0xdcbb14a3;
-//         let except2 = Instruction::Stype(Stype { rs1: 22, rs2: 11, funct3: 1, imm: -567 });
+        #[test]
+        fn m_test() {
+            // mulhsu x17, x16, x15
+            let ins1 = 0x02f828b3;
+            let expect1 = build_m_data(MOp::Mulhsu, 17, 16, 15, 0);
+            // rem x23, x24, x25
+            let ins2 = 0x039c6bb3;
+            let expect2 = build_m_data(MOp::Rem, 23, 24, 25, 0);
 
-//         assert_eq!(decoder::decode(ins1), Ok(except1));
+            assert_eq!(decode(ins1), Ok(expect1));
+            assert_eq!(decode(ins2), Ok(expect2));
+        }
+    }
 
-//         assert_eq!(decoder::decode(ins2), Ok(except2));
-//     }
+    mod zicsr {
+        use crate::decoder::decode;
+        use crate::instruction::{Instruction, InstructionData};
+        use crate::instruction::ZicsrOp;
+        
+        fn build_zicsr_data(op: ZicsrOp, rd: u8, rs1: u8, rs2: u8, imm: i32) -> Instruction {
+            let data = InstructionData { rd, rs1, rs2, imm };
+            Instruction::Ziscr(op, data)
+        }
+        
+        #[test]
+        fn zicsr_test() {
+            // csrrw x0, mstatus, x5
+            let ins1 = 0x30029073;
+            let expect1 = build_zicsr_data(ZicsrOp::Csrrw, 0, 5, 0, 0x300);
+            // csrrci x0, mepc, 6
+            let ins2 = 0x34137073;
+            let expect2 = build_zicsr_data(ZicsrOp::Csrrci, 0, 6, 1, 0x341);
 
-//     #[test]
-//     // Test I-type Jump instruction
-//     fn jtype_test() {
-//         // jal x1, 140
-//         let ins1 = 0x046000ef;
-//         let except1 = Instruction::Jtype(Jtype { rd: 1, imm: 70 });
-//         // jal x0, -32
-//         let ins2 = 0xff1ff06f;
-//         let except2 = Instruction::Jtype(Jtype { rd: 0, imm: -16 });
+            assert_eq!(decode(ins1), Ok(expect1));
+            assert_eq!(decode(ins2), Ok(expect2));
+        }
+    }
 
-//         assert_eq!(decoder::decode(ins1), Ok(except1));
+    mod zifencei {
+        use crate::decoder::decode;
+        use crate::instruction::{Instruction, InstructionData};
+        use crate::instruction::ZifenceiOp;
+              
+        #[test]
+        fn zifencei_test() {
+            // fence.i
+            let ins = 0x0000100f;
+            let expect = 
+                Instruction::Zifencei(ZifenceiOp::FenceI,  
+                    InstructionData { rd: 0, rs1: 0, rs2: 0, imm: 0 });
 
-//         assert_eq!(decoder::decode(ins2), Ok(except2));
-//     }
+            assert_eq!(decode(ins), Ok(expect));
+        }
+    }
 
-//     #[test]
-//     // Test I-type Jump instruction
-//     fn utype_test() {
-//         // auipc x29, 0x100
-//         let ins1 = 0x00100e97;
-//         let except1 = Instruction::UtypeAUIPC(Utype { rd: 29, imm: 0x100_000 });
-//         // lui x19, 0x20000
-//         let ins2 = 0x200009b7;
-//         let except2 = Instruction::UtypeLUI(Utype { rd: 19, imm: 0x20000_000 });
+    mod privileged {
+        use crate::decoder::decode;
+        use crate::instruction::{Instruction, PrivilegeOp};
 
-//         assert_eq!(decoder::decode(ins1), Ok(except1));
+        #[test]
+        fn privileged_test() {
+            // sret
+            let ins1 = 0x10200073;
+            let expect1 = Instruction::Privileged(PrivilegeOp::Sret);
+            // mret
+            let ins2 = 0x30200073;
+            let expect2 = Instruction::Privileged(PrivilegeOp::Mret);
 
-//         assert_eq!(decoder::decode(ins2), Ok(except2));
-//     }
+            assert_eq!(decode(ins1), Ok(expect1));
+            assert_eq!(decode(ins2), Ok(expect2));
+        }
+    }
+    
+    use crate::decoder::decode;
+    use crate::error::DecodeError;
+    use crate::opcode::OpCode;
 
-//     #[test]
-//     // Test illegal opcode
-//     fn err_test() {
-//         let err_ins1 = 0xffffffff;
-//         let err_ins2 = 0x0;
+    #[test]
+    fn illegal_test() {
+        let err_ins1 = 0xffffffff;
+        let expect_err1 = Err(DecodeError::UnknownOpcode(0x7f));
+        let err_ins2 = 0x00200073;
+        let expect_err2 = Err(DecodeError::UnknownInstruction(OpCode::ItypeSystem, 0x00200073));
 
-//         assert_eq!(
-//             decoder::decode(err_ins1),
-//             Err(Exception::IllegalInstruction)
-//         );
-//         assert_eq!(
-//             decoder::decode(err_ins2),
-//             Err(Exception::IllegalInstruction)
-//         );
-//     }
-// }
+        assert_eq!(decode(err_ins1), expect_err1);     
+        assert_eq!(decode(err_ins2), expect_err2);
+    }
+}
